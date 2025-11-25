dffb3261fa7e663ff267d5be5e4b1751ebad38cd
zhou123445 <yiyi0307zyx@qq.com>
2025-11-25 20:27:40 +0800

    Part3-2: Add partThree demo in AssignmentTwo for queue testing
    Implement partThree method to test queue operations (add, remove, print visitors); verify function correctness.

--- src/com/scu/prvms/AssignmentTwo.java
+++ src/com/scu/prvms/AssignmentTwo.java
@@ -5,11 +5,32 @@ package com.scu.prvms;
  */
 public class AssignmentTwo {
     public static void main(String[] args) {
-        // Demo integration will be added in subsequent modules
+        AssignmentTwo demo = new AssignmentTwo();
+        demo.partThree();
     }
 
     /** Demo method for Part 3: Queue Management */
-    public void partThree() {}
+    public void partThree() {
+        System.out.println("\n=== Part3 Queue Management Demo ===");
+        Employee operator = new Employee("E001", "Mr. Zhang", "13800138000", "EMP001", "Morning");
+        Ride rollerCoaster = new Ride("R001", "Roller Coaster", operator, 2);
+
+        Visitor v1 = new Visitor("V001", "Xiao Ming", "13900139001", "T001", true);
+        Visitor v2 = new Visitor("V002", "Xiao Hong", "13900139002", "T002", false);
+        Visitor v3 = new Visitor("V003", "Xiao Gang", "13900139003", "T003", true);
+        Visitor v4 = new Visitor("V004", "Xiao Li", "13900139004", "T004", false);
+        Visitor v5 = new Visitor("V005", "Xiao Qiang", "13900139005", "T005", true);
+        rollerCoaster.addVisitorToQueue(v1);
+        rollerCoaster.addVisitorToQueue(v2);
+        rollerCoaster.addVisitorToQueue(v3);
+        rollerCoaster.addVisitorToQueue(v4);
+        rollerCoaster.addVisitorToQueue(v5);
+
+        rollerCoaster.printQueue();
+        rollerCoaster.removeVisitorFromQueue();
+        System.out.println("\n=== After Removal ===");
+        rollerCoaster.printQueue();
+    }
 
     /** Demo method for Part 4A: Ride History Management */
     public void partFourA() {}
